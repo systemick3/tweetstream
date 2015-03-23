ce709bfcbe988ddf9cff1154df244d1f44a35788
Mike Garthwaite <michael@michael.(none)>
2015-03-23 11:39:51 +0000

    Made trends refresh after 1 hour

--- app/components/trend/trendControllers.js
+++ app/components/trend/trendControllers.js
@@ -1,11 +1,32 @@
 var app = angular.module('twitterapp');
 
-app.controller('trendCtrl', ['$scope', 'trendFactory', function ($scope, trendFactory) {
-  trendFactory.getTrends()
-    .success(function (data) {
-      $scope.trends = data.data[0].trends;
-    })
-    .error(function (err) {
-      $scope.trendsError = 'Unable to get latest trends from Twitter. Please try again later.';
-    });
+app.controller('trendCtrl', ['$scope', '$rootScope', 'trendFactory', 'userFactory', function ($scope, $rootScope, trendFactory, userFactory) {
+
+  var getTrends = function () {
+    var msg = 'Loading new trends.';
+
+    if ($rootScope.streamFilters.length > 0) {
+      msg += ' Your filter will be reset.'
+    }
+
+    $rootScope.streamMessages.push({'type': 'info', 'msg': msg});
+
+    $rootScope.streamFilters = [];
+    $rootScope.streamFilterText = 'No filter set.'
+
+    trendFactory.getTrends()
+      .success(function (data) {
+        $scope.trends = data.data[0].trends;
+        setTimeout(getTrends, 60000);
+      })
+      .error(function (err) {
+        $scope.trendsError = 'Unable to get latest trends from Twitter. Please try again later.';
+        setTimeout(getTrends, 2000);
+      });
+  };
+
+  userFactory.userSessionData().then(function () {
+    getTrends();
+  })
+
 }]);
